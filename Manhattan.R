62c5ba32228fa472f9be74a3a7f0f0b02f3410cf
arsalanstillwell <46775364+arsalanstillwell@users.noreply.github.com>
2021-06-11 11:57:55 -0400

    Fixed error in rearranging columns

--- Manhattan.R
+++ Manhattan.R
@@ -1,18 +1,17 @@
 #Load the sample data
-setwd("/Users/arsalan/Desktop/")
+setwd("C:/Users/rstil2/Downloads")
 twol<-read.table("sample.cmh", header = FALSE)
 
 #Name and rearrange columns
 colnames(twol) <- c("BP", "CHR", "SNP", "P")
-twol <- twol[c(1,3,2,4)]
+twol <- twol[c(3,2,1,4)]
 
 levels(twol$CHR) <- sub("2L", "1", levels(twol$CHR))
 
 #Transform column from character to numeric values
-twol$CHR <- as.numeric(twol$CHR)
-is.numeric(twol$CHR)
+twol$CHR <- 2
 
 #Create a grey/black manhattan plot of the p-values from the CMH test
-install.packages("qqman")
+#install.packages("qqman")
 require(qqman)
 manhattan(twol, suggestiveline = F, genomewideline = F)
